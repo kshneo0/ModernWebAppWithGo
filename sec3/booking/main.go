13d808d4a28b7010d763057dedee0dcd4948b5de
kshneo0 <kshneo@gmail.com>
2022-01-28 09:51:15 +0900

    sec3 ch24 Error checking

--- sec3/booking/main.go
+++ sec3/booking/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -18,6 +19,26 @@ func About(w http.ResponseWriter, r *http.Request) {
 	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
 }
 
+// Divide is the about page handler
+func Divide(w http.ResponseWriter, r *http.Request) {
+	var x, y float32 = 100.0, 0.0
+	f, err := divideValues(x, y)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot devide by 0\n")
+		return
+	}
+	fmt.Fprintf(w, fmt.Sprintf("%f divide by %f is %f", x, y, f))
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("Cannot divide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 // addValues adds two integers and return the sum
 func addValues(x, y int) int {
 	return x + y
@@ -27,6 +48,7 @@ func addValues(x, y int) int {
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Startung application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
