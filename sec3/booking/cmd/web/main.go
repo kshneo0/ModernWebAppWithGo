e01cb321ab7a518c3555ee0aaac23f999688f60d
kshneo0 <kshneo@gmail.com>
2022-01-28 11:11:45 +0900

    sec3 ch28 Enabling Go Modules and refectoring our code to use packages

--- sec3/booking/cmd/web/main.go
+++ sec3/booking/cmd/web/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/ModernWebAppWithGo/sec3/booking/pkg/handlers"
 )
 
 const portNumber = ":8080"
 
 // main is the main application
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("Startung application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
