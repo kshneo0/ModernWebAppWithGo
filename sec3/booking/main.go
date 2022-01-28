31928a115bd7842a18b8342cff65d89d0ea9113d
kshneo0 <kshneo@gmail.com>
2022-01-28 09:38:54 +0900

    sec3 ch23 Functions and handlers

--- sec3/booking/main.go
+++ sec3/booking/main.go
@@ -5,16 +5,29 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+// Home is the about page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+}
+
+// addValues adds two integers and return the sum
+func addValues(x, y int) int {
+	return x + y
+}
+
+// main is the main application
 func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, world!")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
-
-	_ = http.ListenAndServe(":8080",nil)
-
+	fmt.Println(fmt.Sprintf("Startung application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 }
