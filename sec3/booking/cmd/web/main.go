1f30b552edded055779ae61d1f76ca89b3dead2a
kshneo0 <kshneo@gmail.com>
2022-01-28 17:39:53 +0900

    sec4 ch38 Developing our own middleware

--- sec3/booking/cmd/web/main.go
+++ sec3/booking/cmd/web/main.go
@@ -15,13 +15,13 @@ const portNumber = ":8080"
 // main is the main application
 func main() {
 	var app config.AppConfig
-	
+
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
 	}
 	app.TemplateCache = tc
-	app.UseCache = false
+	app.UseCache = true
 
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
