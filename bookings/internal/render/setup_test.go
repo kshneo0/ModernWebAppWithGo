f553fb2c581da10a1431a4dd5dc1d92f8f28a9ab
kshneo0 <kshneo@gmail.com>
2022-01-30 17:45:47 +0900

    sec9 ch74 Writing tests for our Render package 1

---
+++ bookings/internal/render/setup_test.go
@@ -0,0 +1,38 @@
+package render
+
+import (
+	"encoding/gob"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/ModernWebAppWithGo/bookings/internal/config"
+	"github.com/ModernWebAppWithGo/bookings/internal/models"
+	"github.com/alexedwards/scs/v2"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+
+	//change this to true when in production
+	testApp.InProduction = false
+
+	//session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false
+
+	testApp.Session = session
+
+	app = &testApp
+
+	os.Exit(m.Run())
+}
