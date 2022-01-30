cc07bc1a15dd0afdab481d17cdc05c3b9b6063b1
kshneo0 <kshneo@gmail.com>
2022-01-30 16:10:36 +0900

    sec9 ch72 Writing tests for our GET handlers

--- bookings/internal/render/render.go
+++ bookings/internal/render/render.go
@@ -16,6 +16,7 @@ import (
 var functions = template.FuncMap{}
 
 var app *config.AppConfig
+var pathToTemplates = "./templates"
 
 // NetTemplates sets the config for the template package
 func NewTemplates(a *config.AppConfig) {
@@ -76,13 +77,13 @@ func CreateTemplateCache() (map[string]*template.Template, error) {
 			return myCache, err
 		}
 
-		matches, err := filepath.Glob("./templates/*layout.tmpl")
+		matches, err := filepath.Glob(fmt.Sprintf("%s/*layout.tmpl", pathToTemplates))
 		if err != nil {
 			return myCache, err
 		}
 
 		if len(matches) > 0 {
-			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			ts, err = ts.ParseGlob(fmt.Sprintf("%s/*.layout.tmpl", pathToTemplates))
 			if err != nil {
 				return myCache, err
 			}
