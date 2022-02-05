0ec6f100242d80ed907b0cdb21d637060febb104
kshneo0 <kshneo@gmail.com>
2022-02-05 17:18:09 +0900

    sec17 ch146 Listing new reservations

--- bookings/internal/handlers/handlers.go
+++ bookings/internal/handlers/handlers.go
@@ -523,7 +523,17 @@ func (m *Repository) AdminAllReservations(w http.ResponseWriter, r *http.Request
 // AdminNewReservations shows all new reservations in admin tool
 func (m *Repository) AdminNewReservations(w http.ResponseWriter, r *http.Request) {
 
-	render.Template(w, r, "admin-new-reservations.page.tmpl", &models.TemplateData{})
+	reservations, err := m.DB.AllNewReservations()
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	data := make(map[string]interface{})
+	data["reservations"] = reservations
+	render.Template(w, r, "admin-new-reservations.page.tmpl", &models.TemplateData{
+		Data: data,
+	})
 }
 
 // AdminReservationsCalendar displays the reservation calendar
