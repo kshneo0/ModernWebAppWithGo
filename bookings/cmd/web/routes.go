f71aea40507a5343942cb2d02c10bb28ae21fd39
kshneo0 <kshneo@gmail.com>
2022-02-06 14:12:16 +0900

    sec17 ch151 Deleting a reservation

--- bookings/cmd/web/routes.go
+++ bookings/cmd/web/routes.go
@@ -48,6 +48,7 @@ func routes(app *config.AppConfig) http.Handler {
 		mux.Get("/reservations-all", handlers.Repo.AdminAllReservations)
 		mux.Get("/reservations-calendar", handlers.Repo.AdminReservationsCalendar)
 		mux.Get("/process-reservation/{src}/{id}/do", handlers.Repo.AdminProcessReservation)
+		mux.Get("/delete-reservation/{src}/{id}/do", handlers.Repo.AdminDeleteReservation)
 
 		mux.Get("/reservations/{src}/{id}/show", handlers.Repo.AdminShowReservation)
 		mux.Post("/reservations/{src}/{id}", handlers.Repo.AdminPostShowReservation)
