2e91490a72bb089a4854fe619b5df74c0fe0583b
kshneo0 <kshneo@gmail.com>
2022-02-06 16:00:24 +0900

    sec17 ch157 Handling Calendar changes 2

--- bookings/internal/repository/repository.go
+++ bookings/internal/repository/repository.go
@@ -28,5 +28,4 @@ type DatabaseRepo interface {
 	UpdateProcessedForReservation(id, processed int) error
 	AllRooms() ([]models.Room, error)
 	GetRestrictionsForRoomByDate(roomID int, start, end time.Time) ([]models.RoomRestriction, error)
-
 }
